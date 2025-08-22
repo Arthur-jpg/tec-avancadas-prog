f2ba937d4c9c410ac25b6269284a5301a64634ec
Arthur Schiller <arthurschiller10@gmail.com>
2025-08-22 11:39:00 -0300

    final aula

---
+++ 22.08/projeto1/src/control/Programa.java
@@ -0,0 +1,23 @@
+package control;
+
+import model.Curso;
+import model.Disciplina;
+import model.Turma;
+
+public class Programa {
+    public static void main(String[] args) {
+        Curso curso = new Curso("Engenharia de Software");
+        Disciplina di = new Disciplina("Testecodigo", "Tecnicas avancadas", 80);
+        Turma turma = new Turma("Codigo turma", "segunda", di);
+
+        curso.addDisciplina(di);
+        di.addCurso(curso);
+        di.addTurma(turma);
+
+        // Imprime somente o nome das disciplinas do curso engenharia de software
+        System.out.println("Disciplinas do curso " + curso.getNome() + ":");
+        for (Disciplina disciplina : curso.getDisciplinas()) {
+            System.out.println(disciplina.getNome());
+        }
+    }
+}
